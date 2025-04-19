f62475f4a56cee0155d10f1bf2a1179e50dae201
Abhishek <abhishekmaddheshiya632@gmail.com>
2025-04-19 12:01:30 +0530

    day-1

---
+++ app.js
@@ -0,0 +1,33 @@
+// *************************************************** day-1 **********************************************************
+
+
+// Problem: Do numbers ko add karke unka sum print karo.Example: agar input 5 aur 10 hai, toh output 15 hona chahiye.
+// const sum = (a, b) => a + b;
+// console.log(sum(5, 10));
+
+
+// Problem: Ek number diya gaya hai, check karo ki woh even hai ya odd.Agar number even hai, toh print "Even", agar odd hai toh print "Odd".
+// const checkNumber = (num) => {
+//     if (num % 2 === 0) {
+//         console.log("Even");
+//     } else {
+//         console.log("Odd");
+//     }
+// }
+// checkNumber(8)
+
+
+// Problem: Ek number diya gaya hai, uska factorial calculate karo.Example: agar input 4 hai, toh output 24 hoga(4! = 4 * 3 * 2 * 1).
+// const findFactorial = (num) => {
+//     let fact = 1;
+//     for (let i = num; i > 0; i--) {
+//         fact *= i;
+//     }
+//     return fact
+// }
+// console.log(findFactorial(4))
+
+
+
+
+// *************************************************** day-2 **********************************************************
